d53a72525c3f3f2fdf7beecd450746fa7b5bf906
Ali <72344723+alisabourii@users.noreply.github.com>
2023-08-12 12:33:33 +0300

    Add files via upload

---
+++ Joke.cs
@@ -0,0 +1,9 @@
+﻿namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQues { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
