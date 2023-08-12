7cc9357c3bb49775441aa710959c46afb7ac679f
Ali <72344723+alisabourii@users.noreply.github.com>
2023-08-12 12:36:31 +0300

    Add files via upload

---
+++ searchForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JokeWebApp.Views.Jokes
+{
+    public class searchFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
